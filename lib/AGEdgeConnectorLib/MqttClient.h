59bcb7d89e97d9230578fe25a4dd69a75c144976
Daon_Sori <smcdidcksthf@naver.com>
2026-01-21 17:01:38 +0900

    2.1.0

--- lib/AGEdgeConnectorLib/MqttClient.h
+++ lib/AGEdgeConnectorLib/MqttClient.h
@@ -21,21 +21,21 @@
 #define MQTT_CONNECT_BAD_CREDENTIALS 4
 #define MQTT_CONNECT_UNAUTHORIZED 5
 
-#define MQTTCONNECT 1 << 4			// Client request to connect to Server
-#define MQTTCONNACK 2 << 4			// Connect Acknowledgment
-#define MQTTPUBLISH 3 << 4			// Publish message
-#define MQTTPUBACK 4 << 4				// Publish Acknowledgment
-#define MQTTPUBREC 5 << 4				// Publish Received (assured delivery part 1)
-#define MQTTPUBREL 6 << 4				// Publish Release (assured delivery part 2)
-#define MQTTPUBCOMP 7 << 4			// Publish Complete (assured delivery part 3)
-#define MQTTSUBSCRIBE 8 << 4		// Client Subscribe request
-#define MQTTSUBACK 9 << 4				// Subscribe Acknowledgment
+#define MQTTCONNECT 1 << 4		// Client request to connect to Server
+#define MQTTCONNACK 2 << 4		// Connect Acknowledgment
+#define MQTTPUBLISH 3 << 4		// Publish message
+#define MQTTPUBACK 4 << 4		// Publish Acknowledgment
+#define MQTTPUBREC 5 << 4		// Publish Received (assured delivery part 1)
+#define MQTTPUBREL 6 << 4		// Publish Release (assured delivery part 2)
+#define MQTTPUBCOMP 7 << 4		// Publish Complete (assured delivery part 3)
+#define MQTTSUBSCRIBE 8 << 4	// Client Subscribe request
+#define MQTTSUBACK 9 << 4		// Subscribe Acknowledgment
 #define MQTTUNSUBSCRIBE 10 << 4 // Client Unsubscribe request
-#define MQTTUNSUBACK 11 << 4		// Unsubscribe Acknowledgment
-#define MQTTPINGREQ 12 << 4			// PING Request
-#define MQTTPINGRESP 13 << 4		// PING Response
+#define MQTTUNSUBACK 11 << 4	// Unsubscribe Acknowledgment
+#define MQTTPINGREQ 12 << 4		// PING Request
+#define MQTTPINGRESP 13 << 4	// PING Response
 #define MQTTDISCONNECT 14 << 4	// Client is Disconnecting
-#define MQTTReserved 15 << 4		// Reserved
+#define MQTTReserved 15 << 4	// Reserved
 
 #define MQTTQOS0 (0 << 1)
 #define MQTTQOS1 (1 << 1)
@@ -45,11 +45,11 @@
 #define MQTT_HEADER_VERSION_LENGTH 7
 
 #define MQTT_CALLBACK_SIGNATURE std::function<void(char *, uint8_t *, unsigned int)> callback
-#define CHECK_STRING_LENGTH(l, s)                              \
+#define CHECK_STRING_LENGTH(l, s)                                \
 	if (l + 2 + strnlen(s, this->bufferSize) > this->bufferSize) \
 	{                                                            \
-		_client->stop();                                           \
-		return false;                                              \
+		_client->stop();                                         \
+		return false;                                            \
 	}
 
 class MqttClient : public Print
